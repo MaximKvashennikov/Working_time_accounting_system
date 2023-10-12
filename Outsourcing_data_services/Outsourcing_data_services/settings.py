cb1aa78895be0e259536a51f78cece35e5a3e0f8
maxim.kvashennikov <kvashennickov.maxim@yandex.ru>
2023-10-12 11:49:46 +0300

    Fixed the tailwind_css path in STATICFILES_DIRS

--- Outsourcing_data_services/Outsourcing_data_services/settings.py
+++ Outsourcing_data_services/Outsourcing_data_services/settings.py
@@ -94,8 +94,7 @@ STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    # BASE_DIR / "static",
-    "../tailwind_css/node_modules/flowbite/dist/",
+    BASE_DIR / "../tailwind_css/node_modules/flowbite/dist/",
 ]
 
 
